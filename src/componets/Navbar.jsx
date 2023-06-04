46ff42a7d6dbe0824575df3638134d2f74556d22
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-06-04 16:54:26 +0600

    navbar added

---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className='grid grid-cols-3 justify-between items-center p-5'>
+            <h1 className='text-3xl font-bold'><Link to='/'>Task Management</Link></h1>
+            <ul className='flex gap-12'>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/all-tasks'>All Task</Link></li>
+                <li><Link to='/add-a-task'>Add A Task</Link></li>
+            </ul>
+            <div className='flex justify-end'>
+               <span><Link to='/'>Profile</Link></span>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../componets/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
-            <h1>Main layout</h1>
+            <Navbar />
             <Outlet />
         </div>
     );
